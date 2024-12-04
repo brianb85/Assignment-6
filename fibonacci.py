6a464eae07d8e0408fdc8975b31b29e28a44d73d
brianb85 <brian.blaker@outlook.com>
2024-12-04 16:58:41 -0500

    Test 2

--- fibonacci.py
+++ fibonacci.py
@@ -2,14 +2,22 @@
 Iterable which produces an iterator of the Fibonacci series for a given value.
 """
 
+
 class Fibonacci:
 
     def __init__(self, value):
         int(value)
         self.value = value
+        self.end = 0
+
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        pass
+        if self.end > self.value:
+            raise StopIteration
+
+        self.end = 1
+        return self.value
+
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -8,3 +8,6 @@ from fibonacci import Fibonacci
 def test_value_is_int():
     with pytest.raises(ValueError):
         Fibonacci("asd")
+
+def test_value_0():
+    assert list(Fibonacci(0)) == [0]
